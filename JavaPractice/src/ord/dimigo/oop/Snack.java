dfb5a3131a432f6531c99011075092ed4eb0e294
HD2627 <sweetingso@naver.com>
2015-05-21 09:57:32 +0900

    ㅁ너아ㅣㅁㄴ업전ㅁㅇㅂㅈ

---
+++ JavaPractice/src/ord/dimigo/oop/Snack.java
@@ -0,0 +1,62 @@
+package ord.dimigo.oop;
+
+public class Snack {
+
+	private String name;
+	private String company;
+	private int price;
+	private int number;
+	
+	public Snack(){
+		
+	}
+	
+	public Snack(String name,String company,int price,int number){
+		this.name=name;
+		this.company=company;
+		this.price=price;
+		this.number=number;
+		}
+	public String getName() {
+		return name;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public int getPrice() {
+		return price;
+	}
+	
+	public int getNumber() {
+		return number;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
+	public void printSnack(){
+		System.out.println("이름 : "+name);
+		System.out.println("제조사 : "+company);
+		System.out.println("가격 : "+String.format("%,d",price)+"원");
+		System.out.println("개수 : "+number+"개");
+	}
+	
+	public int calcPrice(){
+		return number*price;
+		
+	}
+
+}
