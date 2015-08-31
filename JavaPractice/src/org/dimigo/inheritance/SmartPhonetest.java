c2ef401dbd9b08d0b5d774f99592bf0191af5834
HD2627 <sweetingso@naver.com>
2015-08-31 09:08:07 +0900

    dasd

---
+++ JavaPractice/src/org/dimigo/inheritance/SmartPhonetest.java
@@ -0,0 +1,22 @@
+package org.dimigo.inheritance;
+
+public class SmartPhonetest {
+	public static void main(String[] args) {
+		SmartPhone i = new IPhone("iPhone 6", "애플", 700000);
+		SmartPhone g = new Galaxy("갤럭시 S6", "삼성", 650000);
+
+		System.out.println(i);
+		i.turnOn();
+		i.pay();
+		i.useSpecialFunction();
+		i.turnOff();
+		
+		System.out.println();
+		
+		System.out.println(g);
+		g.turnOn();
+		g.pay();
+		g.useSpecialFunction();
+		g.turnOff();
+	}
+}
