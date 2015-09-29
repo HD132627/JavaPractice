fc30b13bc9c468cc80c5ab53b377c0b0c7143da9
HD2627 <sweetingso@naver.com>
2015-09-29 20:41:59 +0900

    성

--- JavaPractice/src/org/dimigo/exception/AgeCheckException.java
+++ JavaPractice/src/org/dimigo/exception/AgeCheckException.java
@@ -1,10 +1,9 @@
 package org.dimigo.exception;
 
 public class AgeCheckException extends Exception{
-	public AgeCheckException() {
-		
+	public AgeCheckException() {}
+	public AgeCheckException(Movie movie){
+		super(movie.getTitle() + "은/는 "+movie.getLimitAge()+"이상 관람가 입니다.");
 	}
-	public AgeCheckException(Movie movie) {
-		super(movie.getTitle() + "은/는 " + movie.getLimitAge() + "이상 관람가입니다.");
-	}
+	
 }
